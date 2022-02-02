ff3bf45a2e01a0181aee5db190afde024c85ef58
Nikita Zarutskiy <Aboba410@yandex.ru>
2022-02-02 19:05:57 +0500

    feat:fixJS

--- scripts/index.js
+++ scripts/index.js
@@ -23,6 +23,10 @@ const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 const cardImage = cardElement.querySelector('.card__image');
 const popupAddCards = document.querySelector('.popup-add-card');
 const popupBtnAddCardsClose = popupAddCards.querySelector('.popup__botton-close');
+const imagePopupPreview = popupAddCards.querySelector('.popup__image')
+const titlePopupPreview = popupAddCards.querySelector('.popup__text')
+const titleInputValue = popupAddCard.querySelector('.popup__input-name')
+const descriptionInputValue = popupAddCard.querySelector('.popup__input-name_type_user-job')
 
 // Делаем шаблон для всех popup (Открытия попап)
 function openPopup(popup) {
@@ -59,7 +63,7 @@ function createCard (name, link) {
   const cardTitle = cardElement.querySelector('.card__title');
   cardTitle.textContent = name;   // Присваиваем заголовку карточки аргумент функции
   cardImage.src = link;    // Присваиваем фото карточки аргумент функции
-  cardImage.alt = cardTitle.textContent; // Присваиваем альт карточки аргумент функции
+  cardImage.alt = name; // Присваиваем альт карточки аргумент функции
   cardElement.querySelector('.card__heart').addEventListener('click', function(evt){
     evt.target.classList.toggle('card__heart_active'); // Делаем лайк карточке
   });
@@ -68,27 +72,27 @@ function createCard (name, link) {
   });
   cardImage.addEventListener('click', function(){  // Попап для открытия попапа прямо с карточки
     openPopup(popupAddCards);
-    popupAddCards.querySelector('.popup__image').src = link; // Присваиваем картинке попапа аргумент функции
-    popupAddCards.querySelector('.popup__text').textContent = name; // Присваиваем тексту попапа аргумент функции
-    popupAddCards.querySelector('.popup__image').alt = name; // Присваиваем альт попапа аргунт функции
+    imagePopupPreview.src = link; // Присваиваем картинке попапа аргумент функции
+    titlePopupPreview.textContent = name; // Присваиваем тексту попапа аргумент функции
+    imagePopupPreview.alt = name; // Присваиваем альт попапа аргунт функции
   })
   popupBtnAddCardsClose.addEventListener('click', function(){
     closePopup(popupAddCards);
   })
-  sectionCards.prepend(cardElement) // Делаем карточку видимой
+  return cardElement; // Делаем карточку видимой
 }
 
 // Рендерим карточки передавая аргументы функции которую мы уже создали
 initialCards.forEach(function(item) {
-  createCard(item.name, item.link);
+  sectionCards.prepend(createCard(item.name, item.link));
 })
 
 function handleCardFormSubmit(evt) {
   evt.preventDefault();
-  const titleInputValue = popupAddCard.querySelector('.popup__input-name').value
-  const descriptionInputValue = popupAddCard.querySelector('.popup__input-name_type_user-job').value
-  createCard(titleInputValue, descriptionInputValue);
+  sectionCards.prepend(createCard(titleInputValue.value, descriptionInputValue.value));
   closePopup(popupAdd);
+  titleInputValue.value = '';
+  descriptionInputValue.value = '';
 }
 
 function handleProfileFormSubmit(evt) {
